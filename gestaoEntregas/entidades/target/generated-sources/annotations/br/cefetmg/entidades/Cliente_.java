69c97da03030da371a14d6242beb3ae5d45f9936
Matheus-deAssis <matheusamaral752@gmail.com>
2024-08-21 22:23:52 -0300

    Partes do View

--- gestaoEntregas/entidades/target/generated-sources/annotations/br/cefetmg/entidades/Cliente_.java
+++ gestaoEntregas/entidades/target/generated-sources/annotations/br/cefetmg/entidades/Cliente_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-08-19T08:31:51", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-08-21T21:30:21", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
--- gestaoEntregas/view/src/main/java/br/cefetmg/view/CadastrarClienteController.java
+++ gestaoEntregas/view/src/main/java/br/cefetmg/view/CadastrarClienteController.java
@@ -1,26 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package br.cefetmg.view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Aluno
- */
-public class CadastrarClienteController implements Initializable {
+public class CadastrarClienteController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    private Label BairroLabel;
+
+    @FXML
+    private TextField BairroText;
+
+    @FXML
+    private Label CnpjLabel;
+
+    @FXML
+    private TextField CnpjText;
+
+    @FXML
+    private Label LograLabel;
+
+    @FXML
+    private TextField LograText;
+
+    @FXML
+    private Label NomeLabel;
+
+    @FXML
+    private TextField NomeText;
+
+    @FXML
+    private Button Salvar;
+
+    @FXML
+    private Label TelLabel;
+
+    @FXML
+    private TextField TelText;
+
+    @FXML
+    private Label Titulo;
+
+    @FXML
+    private Label cpfLabel;
+
+    @FXML
+    private TextField cpfText;
+
+    @FXML
+    void onSalvar(ActionEvent event) {
+        if (NomeText.getText().isEmpty() || BairroText.getText().isEmpty() || CnpjText.getText().isEmpty() ||
+            LograText.getText().isEmpty() || TelText.getText().isEmpty() || cpfText.getText().isEmpty()) {
+            exibirAlerta(AlertType.ERROR, "Erro", "Preencha todos os campos!");
+        } else {
+            exibirAlerta(AlertType.INFORMATION, "Sucesso", "Cliente cadastrado com sucesso!");
+        }
+    }
+
+    private void exibirAlerta(AlertType tipo, String titulo, String mensagem) {
+        Alert alerta = new Alert(tipo);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensagem);
+        alerta.showAndWait();
+    }
 }
